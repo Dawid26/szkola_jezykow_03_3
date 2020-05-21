4e757c465b41a70b75b0598476f9913fc3b70c89
Dawid Mariankowski <dawid24.9@o2.pl>
2020-05-21 16:35:29 +0200

    hello-1

--- wszystkie_zamowienia.php
+++ wszystkie_zamowienia.php
@@ -36,58 +36,55 @@ FROM student_has_kurs INNER JOIN
      student_has_kurs.student_student_id = '$studentId'
     ";
 ?>
-<div class="container">
-    <div class="col-4 text-center center-block">
-        <a href="wszystkie-kursy.php">
-            <button type="button" class="btn btn-success btn-lg btn-block m-4">
-                Zobacz więcej kursów
-            </button>
-        </a>
-    </div>
-    <?php
-    $results = $db_connection->query($query);
-    foreach ($results as $result) {
-        ?>
-        <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-                <div class="card-body">
-                    <p class="card-text">nazwa:<b> <?php echo $result['nazwa'] ?></b></p>
-                    <p class="card-text">Kurs id: <?php echo $result['kurs_kurs_id'] ?></p>
-                    <p class="card-text">Cena: <?php echo $result['cena'] ?></p>
-                    <p class="card-text">Ostatniawplata: <?php echo $result['ostatnia_wplata'] ?></p>
-                    <p class="card-text">Data zamówienia: <?php echo $result['data_utworzenia'] ?></p>
-
-                    <p class="card-text">odnawialny:
-                        <?php echo ' ' . ($result['odnawialny'] ? $result['odnawialny'] : ' ' . 'Nie'); ?>
-                    </p>
-                    <p class="card-text">ostatnia naleznosc:
-                        <?php echo ' ' . ($result['ostatnia_naleznosc'] ? $result['ostatnia_naleznosc'] : 'Brak') . ''; ?>
-                    </p>
-                    <p class="card-text"><b>oplacony:
-                        <?php echo ' ' . ($result['oplacony'] ? $result['oplacony'] : 'Nie') . ''; ?>
-                        </b> </p>
-                    <?php
-?>
-                    <p class="card-text"><b>Pozostało do zapacenia:
-                        <?php echo ' ' . (!$result['oplacony'] ? $result['cena'] : 'Brak zadłużenia') . ''; ?>
-                        </b> </p>
-                    <p>
-                        <a style="color:white;" href="dodaj-opinie.php?kurs_id=<?php echo $result['kurs_kurs_id'] ?>
-                               &amp;student_id=<?php echo $studentId ?>">
-                            <button type="submit" class="btn btn-primary mt-2 ml-1">
-                                Dodaj Opinie
-                            </button>
-                        </a>
-                    </p>
-                </div>
-            </div>
-
+    <div class="container">
+        <div class="col-4 text-center center-block">
+            <a href="wszystkie-kursy.php">
+                <button type="button" class="btn btn-success btn-lg btn-block m-4">
+                    Zobacz więcej kursów
+                </button>
+            </a>
         </div>
         <?php
-    }
-    ?>
-</div>
-<p></p></br><?php
+        $results = $db_connection->query($query);
+        foreach ($results as $result) {
+            ?>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <p class="card-text">nazwa:<b> <?php echo $result['nazwa'] ?></b></p>
+                        <p class="card-text">Kurs id: <?php echo $result['kurs_kurs_id'] ?></p>
+                        <p class="card-text">Cena: <?php echo $result['cena'] ?></p>
+                        <p class="card-text">Ostatnia wplata:
+                            <?php echo ' ' . ((int)$result['ostatnia_wplata'] ? $result['ostatnia_wplata'] : ' ' . 'Brak'); ?>
+                        </p>
+                        <p class="card-text">Data zamówienia: <?php echo $result['data_utworzenia'] ?></p>
+
+                        <p class="card-text">odnawialny:
+                            <?php echo ' ' . ((int)$result['odnawialny'] ? (int)$result['odnawialny'] : ' ' . 'Nie'); ?>
+                        </p>
+                        <p class="card-text"><b>oplacony:
+                                <?php echo ' ' . ((int)$result['oplacony'] ? (int)$result['oplacony'] : 'Nie') . ''; ?>
+                        </p></b>
+                        <p class="card-text"><b>Pozostało do zapacenia:
+                                <?php echo ' ' . (!(int)$result['oplacony'] ? $result['cena'] : 'Brak zadłużenia') . ''; ?>
+                            </b> </p>
+                        <p>
+                            <a style="color:white;" href="dodaj-opinie.php?kurs_id=<?php echo $result['kurs_kurs_id'] ?>
+                               &amp;student_id=<?php echo $studentId ?>">
+                                <button type="submit" class="btn btn-primary mt-2 ml-1">
+                                    Dodaj Opinie
+                                </button>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+    <p></p></br><?php
 include('footer.php');
 $polaczenie->close();
 ?>
